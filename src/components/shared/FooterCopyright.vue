77184b7970ad2b1877c6dca81303ace6375df8f0
RemoSpys <remotammela@gmail.com>
2024-10-17 12:30:08 +0300

    Initial commit with fresh start

---
+++ src/components/shared/FooterCopyright.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+	data: () => {
+		return {
+			copyrightDate: new Date().getFullYear(),
+			projectName: 'Vue.js & Tailwind CSS Portfolio',
+			author: 'Stoman',
+		};
+	},
+};
+</script>
+
+<template>
+	<div class="flex justify-center items-center text-center">
+		<div
+			class="font-general-regular text-lg text-ternary-dark dark:text-ternary-light"
+		>
+			&copy; {{ copyrightDate }}.
+			<a
+				href="https://github.com/D1ZEASE420/Portfolio-Repo"
+				target="__blank"
+				class="font-general-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500"
+			>
+				{{ projectName }}
+			</a>
+			. Developed by D1ZEASE420 & RemoSpys
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/data/socialLinks.js
@@ -0,0 +1,32 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: 'https://www.ametikool.ee/et',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/D1ZEASE420/Portfolio-Repo',
+	},
+	{
+		id: 3,
+		name: 'Twitter',
+		icon: 'twitter',
+		url: 'https://twitter.com/',
+	},
+	{
+		id: 4,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/',
+	},
+	{
+		id: 5,
+		name: 'YouTube',
+		icon: 'youtube',
+		url: 'https://www.youtube.com/watch?v=EScLmWJs82I',
+	},
+];
